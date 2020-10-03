27fb6fdbcf00ef3cf5ae171a718f2e7f71fd1f26
ankitsejwal <ankit@sejw.al>
2020-10-03 20:24:12 +1000

    finished setting up static files and logo

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const expressLayouts = require("express-ejs-layouts");
+const home = require("./routes/home");
 const donors = require("./routes/donors");
 const seekers = require("./routes/seekers");
 
@@ -17,11 +18,13 @@ const app = express();
 
 app.use(expressLayouts);
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
 
 app.set("view engine", "ejs");
 app.set("layout", "layouts/layout");
 
 // use routes
+app.use("/", home);
 app.use("/donors", donors);
 app.use("/seekers", seekers);
 
